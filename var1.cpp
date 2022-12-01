6d4ddeeff38982d7d69e900f0b7f5beb01cfdfe8
Nikita Syutkin <syutkin.program@gmail.ru>
2022-12-01 21:29:05 +0700

    delenie

--- var1.cpp
+++ var1.cpp
@@ -3,9 +3,11 @@ using namespace std;
 #include<windows,h>
 int main(void)
 {
-	int a = 3, b = 5, c = 0, d = 0, e = 0;
+	int a = 3, b = 5; 
+	int c = 0, d = 0, e = 0, g = 0;
 	d = a - b;
 	c = a + b;
 	e = a * b
+	g = a / b
 	return 0;
 }
