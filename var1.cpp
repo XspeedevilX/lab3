4816b2f8227c995027764161c51b34302b4175fc
Nikita Syutkin <syutkin.program@gmail.ru>
2022-12-01 21:26:04 +0700

    proizvedenie

--- var1.cpp
+++ var1.cpp
@@ -3,8 +3,9 @@ using namespace std;
 #include<windows,h>
 int main(void)
 {
-	int a = 3, b = 5, c = 0, d = 0;
+	int a = 3, b = 5, c = 0, d = 0, e = 0;
 	d = a - b;
 	c = a + b;
+	e = a * b
 	return 0;
 }
